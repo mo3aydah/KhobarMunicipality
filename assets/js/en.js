623a61e57aecd917b81b1eab6ad377b8c2478c4e
Atheer Alotaibi <atheerf.otaibi@gmail.com>
2023-06-26 20:04:23 +0300

    Khobar Municipality Eid Request

---
+++ assets/js/en.js
@@ -0,0 +1,56 @@
+var canvas = document.getElementById("myCanvas");
+var context = canvas.getContext("2d");
+
+var imageWidth = 1080;
+var imageHeight = 1080;
+
+var imageObj = new Image(imageWidth,imageHeight);
+
+imageObj.onload = function()
+{
+    context.drawImage(imageObj, 0, 0);
+}
+
+function DownloadCanvasAsImage(){
+    let imageName = "moa3aydah.png";
+    let downloadLink = document.createElement('a');
+    downloadLink.setAttribute('download', imageName);
+    let canvas = document.getElementById('myCanvas');
+    canvas.toBlob(function(blob) {
+    let url = URL.createObjectURL(blob);
+    downloadLink.setAttribute('href', url);
+    downloadLink.click();
+    });
+}
+
+imageObj.src = "assets/images/Eid-Adha-En.jpg"; 
+
+var downloadCardButton = document.getElementById('downloadCard');
+downloadCardButton.addEventListener('click', function(e){
+    // clear canvas from text and draw image Regards,
+    context.clearRect(0, 0, imageWidth, imageHeight);
+    context.drawImage(imageObj, 0, 0);
+
+    var text = document.getElementById('name').value;
+
+    // custom font
+    context.textAlign = 'center';
+    context.font = "40pt Calibri";
+
+    // text color
+    context.fillStyle = '#41B891';
+
+    // center and make text
+    textWidth = (imageWidth - imageWidth / 1.95)
+    textHeight = (imageHeight - 170)
+    
+    context.fillText(text, textWidth, textHeight);
+
+    e.preventDefault();
+    document.getElementById('name').value = "";
+
+    // download the image
+    DownloadCanvasAsImage();
+});
+
+
